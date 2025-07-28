261108bb62ca007c921bbe4a48e3ec9304043efc
renmuming <1652339654@qq.com>
2025-07-28 16:24:17 +0800

    按钮组件

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { ButtonProps } from './types'
+
 defineOptions({
   name: 'RmButton',
 })
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: 'button',
+  nativeType: 'button',
+})
+
+const slots = defineSlots()
+
+const _ref = ref<HTMLButtonElement>()
 </script>
 
 <template>
-  <button style="background-color: blue; color: red">this is button</button>
+  <component
+    :is="props.tag"
+    :ref="_ref"
+    class="rm-button"
+    :type="tag === 'button' ? nativeType : void 0"
+    :disabled="disabled || loading ? true : void 0"
+    :class="{
+      [`rm-button--${type}`]: type,
+      [`rm-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+    }"
+  >
+    <slot></slot>
+  </component>
 </template>
 
 <style scoped lang=""></style>
